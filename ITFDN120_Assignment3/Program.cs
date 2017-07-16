b6f7355046cf45697ff1f762b3cd157ef0c537f5
David Clayton <david@davidclayton.me>
2017-07-15 17:27:15 -0700

    Success

--- ITFDN120_Assignment3/Program.cs
+++ ITFDN120_Assignment3/Program.cs
@@ -6,10 +6,118 @@ using System.Threading.Tasks;
 
 namespace ITFDN120_Assignment3
 {
+    class RPNStack
+    {
+        private int stackPointer = 1;
+        private int[] stack = new int[1];
+
+        //Push a value on top of the stack, after resizing the stack to accomodate
+        public void Push(int value)
+        {
+            stackPointer++;
+            Array.Resize(ref stack, stackPointer);
+            stack[stackPointer - 1] = value;
+        }
+
+        //Pop a value from the stack, and resize the array
+        public int Pop()
+        {
+            if (!IsEmpty())
+            {
+                int value = stack[stackPointer - 1];
+                Array.Resize(ref stack, stackPointer - 1);
+                stackPointer--;
+                return value;
+            }
+            return 0;
+        }
+
+        //Return the top value on the stack
+        public int Top()
+        {
+            if (!IsEmpty())
+            {
+                return stack[stackPointer - 1];
+            }
+            return 0;
+        }
+
+        //If stack pointer is 0, it is empty
+        public bool IsEmpty()
+        {
+            if (stackPointer == 1) return true;
+            return false;
+        }
+    }
+    
+    class RPN
+    {
+        private string[] operators = { "+", "-", "*", "/" };
+        private RPNStack stack = new RPNStack();
+
+        //if input is a number, pushes it onto the stack, if it's an operator, it performs the operation
+        public void Process(string input)
+        {
+            bool isOperator = false;
+            int result = 0;
+
+            for(int i = 0; i < operators.Length; i++)
+            {
+                if (operators[i] == input) isOperator = true;
+            }
+
+            if(isOperator)
+            {
+                int num1 = stack.Pop();
+                int num2 = stack.Pop();
+
+                switch(input)
+                {
+                    case "+":
+                        result = num2 + num1;
+                        break;
+                    case "-":
+                        result = num2 - num1;
+                        break;
+                    case "*":
+                        result = num2 * num1;
+                        break;
+                    case "/":
+                        result = num2 / num1;
+                        break;
+                }
+                stack.Push(result);
+                return;
+            }
+
+            stack.Push(int.Parse(input));
+            return;
+        }
+
+        //returns the top value on the stack
+        public int Result()
+        {
+            return stack.Top();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            RPN rpn = new RPN();
+            bool stop = false;
+            string input;
+
+            while (!stop)
+            {
+                input = Console.ReadLine();
+                if (input.Length == 0) stop = true;
+                else
+                {
+                    rpn.Process(input);
+                    Console.WriteLine("=" + rpn.Result());
+                }
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # ITFDN120_Assignment3
 https://canvas.uw.edu/courses/1166106/assignments
 
+Create a Reverse Polish Notation (RPN) calculator using the Stack class.
+
+rpn = new RPN
+
+while (true)
+{
+  string str = readLineFromKeyboard( )
+
+  rpn.process(str)
+
+  print("=" + rpn.result( ))
+}
+
+Your process( ) function should handle "+", "-", "*", "/", and positive/negative numbers.
+
+When process( ) gets a number, it should push the number onto the stack.
+
+When process( ) gets an operation, it should pop two values off the stack, perform the operation, then push the result back onto the stack. 
+
+The result( ) function should return the last value pushed on the stack.
+
+
+Hint: Your process function should explicitly handle the operations and if it's not an operation, assume it's a number and push that number on the stack.
+
+Scenario: In RPN, "5 ENTER 4 ENTER + ENTER 3 ENTER - ENTER" yields a value of 6.
+
+Calc>5
+=5
+Calc>4
+=4
+Calc>+
+=9
+Calc>3
+=3
+Calc>-
+=6
+Previous Next
